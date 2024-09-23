a327b6861d60e035bdfbe1f17c2df2c8a5691397
StefanZagarov <stefanzagarov@gmail.com>
2024-09-23 12:34:43 +0300

    Setup Express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log(`Howdy!`);
+import express from "express";
+
+const port = 5000;
+
+const app = express();
+
+app.get(`/`, (req, res) =>
+{
+    res.send(`Howdy!`);
+});
+
+app.listen(port, () => console.log(`Server is listening on http://localhost:5000...`));
