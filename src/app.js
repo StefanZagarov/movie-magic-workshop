ec94c191889660bd425dd5ff5149da147ed2975c
StefanZagarov <stefanzagarov@gmail.com>
2024-09-23 13:22:51 +0300

    Setup handlebars

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 import express from "express";
+import handlebars from "express-handlebars";
 
 const port = 5000;
 
+// Get the server
 const app = express();
 
+// Setup the engine
+app.engine(`hbs`, handlebars.engine({
+    extname: `hbs`,
+}));
+
+// Set default engine
+app.set(`view engine`, `hbs`);
+// Set the server to search for the views in the correct path
+app.set(`views`, `./src/views`);
+
+// Add the default route
 app.get(`/`, (req, res) =>
 {
-    res.send(`Howdy!`);
+    res.render(`index`);
 });
 
+// Add a listener
 app.listen(port, () => console.log(`Server is listening on http://localhost:5000...`));
